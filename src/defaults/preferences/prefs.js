7779e8a8dbfa81c01a2fbc3be596f851e54176ad
Georg Koppen <gk@torproject.org>
2018-08-30 20:49:15 +0000

    Bug 27392: Update Moat URLs

--- src/defaults/preferences/prefs.js
+++ src/defaults/preferences/prefs.js
@@ -46,9 +46,9 @@ pref("extensions.torlauncher.torrc_path", "");
 pref("extensions.torlauncher.tordatadir_path", "");
 
 // BridgeDB-related preferences (used for Moat).
-pref("extensions.torlauncher.bridgedb_front", "dontbeevil.appspot.com");
-pref("extensions.torlauncher.bridgedb_reflector", "https://tor-bridges-hyphae-channel.appspot.com");
-pref("extensions.torlauncher.moat_service", "https://bridges.torproject.org/moat");
+pref("extensions.torlauncher.bridgedb_front", "ajax.aspnetcdn.com");
+pref("extensions.torlauncher.bridgedb_reflector", "https://onionquiche.azureedge.net/");
+pref("extensions.torlauncher.moat_service", "https://moat.onionlab.eu/moat");
 pref("extensions.torlauncher.bridgedb_bridge_type", "obfs4");
 
 // Recommended default bridge type (can be set per localized bundle).
