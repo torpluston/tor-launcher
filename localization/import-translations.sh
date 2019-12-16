d95b8fa0448623bdd47b32b8b3ce60274bc3c540
Kathy Brade <brade@pearlcrescent.com>
2019-12-16 16:57:15 -0500

    Bug 32636:  Clean up locales shipped with Tor Launcher
    Synchronize locale names with those that are used on Transifex and
      remove old locales (the Transifex locale names were recently updated
      to match the names that Mozilla uses).
    Limit the locales that we ship to those for which we ship a localized
      Tor Browser. The list is defined via a new BUNDLE_LOCALES variable
      within localization/import-translations.sh (this is the same technique
      that is used by Torbutton).
    Pull in recent translation updates.

--- localization/import-translations.sh
+++ localization/import-translations.sh
@@ -3,6 +3,10 @@
 ### Copyright (c) 2019, The Tor Project, Inc.
 ### See LICENSE for licensing information.
 
+# This var comes from the TBB locale list.
+# XXX: Find some way to keep this, tor-launcher, and Tor Browser in sync
+BUNDLE_LOCALES="ar ca cs da de el es-AR es-ES fa fr ga-IE he hu id is it ja ka ko lt mk ms nb-NO nl pl pt-BR ro ru sv-SE th tr vi zh-CN zh-TW"
+
 TRANSLATION_BRANCHES="
 tor-launcher-network-settings
 tor-launcher-properties
@@ -39,8 +43,7 @@ cd ..
 
 # Remove all locales that are missing one or more string files.
 # Remove all locales for which no translation has been done.
-# Re-create the chrome.manifest and jar.mn files by appending to templates.
-echo "Removing incomplete locales; updating chrome.manifest and jar.mn"
+echo "Removing incomplete locales"
 cd ../src/chrome/locale
 CHROME_MANIFEST=../../../chrome.manifest
 JAR_MN=../../../jar.mn
@@ -54,7 +57,7 @@ for locale in *; do
   omit_locale=0
   for f in ${REQUIRED_TRANSLATION_FILES}; do
     if [ ! -e "${locale}/${f}" ]; then
-      echo "Removing locale ${locale} due to missing resource ${f}"
+      echo "  removing locale ${locale} due to missing resource ${f}"
       omit_locale=1
       break
     fi
@@ -62,18 +65,29 @@ for locale in *; do
   if [ ${omit_locale} -eq 0 ]; then
     diff -r en-US "${locale}" > /dev/null
     if [ $? -eq 0 ]; then
-      echo "Removing locale ${locale} because it has the same content as en-US"
+      echo "  removing locale ${locale} because it has the same content as en-US"
       omit_locale=1
     fi
   fi
   if [ ${omit_locale} -ne 0 ]; then
     rm -rf "${locale}"
-  else
-    echo "locale torlauncher ${locale} chrome/locale/${locale}/" >> \
-        ${CHROME_MANIFEST}
-    echo "% locale torlauncher ${locale} %locale/${locale}/" >> \
-        ${JAR_MN}
-    echo "  locale/${locale}/ (src/chrome/locale/${locale}/*)" >> \
-        ${JAR_MN}
   fi
+done
+
+# Re-create the chrome.manifest and jar.mn files from the BUNDLE_LOCALES list
+# (by appending to templates).
+echo "Updating chrome.manifest and jar.mn"
+echo "  locales to be included in the package: $BUNDLE_LOCALES"
+for locale in $BUNDLE_LOCALES; do
+  if [ ! -d "$locale" ]; then
+    echo "Error: missing locale $locale" 1>&2
+    exit 1
+  fi
+
+  echo "locale torlauncher ${locale} chrome/locale/${locale}/" >> \
+      ${CHROME_MANIFEST}
+  echo "% locale torlauncher ${locale} %locale/${locale}/" >> \
+      ${JAR_MN}
+  echo "  locale/${locale}/ (src/chrome/locale/${locale}/*)" >> \
+      ${JAR_MN}
 done
